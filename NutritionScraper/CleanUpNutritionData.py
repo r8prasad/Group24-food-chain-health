03ac6a37d51ff480a8af1706354d78be3c4bf1cf
vajoshi25 <v1joshi@ucsd.edu>
2019-05-30 17:16:32 -0700

    Merged CSV without the Price coolumn, but with the metrics.

--- NutritionScraper/CleanUpNutritionData.py
+++ NutritionScraper/CleanUpNutritionData.py
@@ -29,3 +29,25 @@ def clean_extracted_data(fname, thresholdSize, thresholdProtein):
 	df4.to_csv(f"{fname}_Clean.csv", encoding = 'iso-8859-1', index=False)
 
 clean_extracted_data(fname="PizzaHut_NutritionData", thresholdSize = 70, thresholdProtein = 2)
+
+def str_to_int(df):
+    """
+    Takes the dataframe as input for the values of the different food stuffs and converts them to float
+    
+    Removes the g and mg at the end.
+    """
+    df = df.str.strip('g')
+    df = df.str.strip('$')
+    df = df.str.strip('m').astype(float)
+    return df
+
+def data_cleaning(df):
+    """
+    Cleaning the data obtained from scraping the websites.
+    Inital pricessing for formatiing the data into the useful format.
+    """
+    assert isinstance(df,pd.DataFrame)
+    df['Calories'] = df['Calories'].astype(str)
+    df = df.set_index('Item').apply(lambda x: str_to_int(x), axis = 1)
+#    df['Serving Size'] = df['Serving Size'].apply(lambda x : 1 if x == 'N/A' else x)
+    return df
